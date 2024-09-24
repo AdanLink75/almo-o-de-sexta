ac43f12796717a520e8e3a9531603b74b26551e9
AdanLink75 <141869449+AdanLink75@users.noreply.github.com>
2024-09-24 12:06:18 -0300

    asd

--- main.js
+++ main.js
@@ -2,22 +2,29 @@ function soma(num1,num2){
     return num1+num2;
 
 }
-console.log(soma(2,3))
+
+console.log(soma(2,3));
 
 function multiplicação(num1,num2){
     return num1*num2;
 }
 
-console.log(multiplicação(6,6))
+console.log(multiplicação(6,6));
 
 function areaquadrado(lado){
     return lado*lado;
 }
 
-console.log(areaquadrado(9))
+console.log(areaquadrado(9));
 
 let areatrapezio=function(num1,num2,num3){
     return (num1+num2)*num3/2;
 }
 
-console.log(areatrapezio(6,17,6))
+console.log(areatrapezio(6,17,6));
+
+let nome=prompt('nome?');
+
+document.write(nome);
+
+parseInt(prompt('nome?'))
