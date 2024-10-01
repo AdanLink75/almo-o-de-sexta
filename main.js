2b3260db69cbf553f4bb4308d0db8a1fca8cad5a
AdanLink75 <141869449+AdanLink75@users.noreply.github.com>
2024-10-01 12:10:40 -0300

    rtfdghjsk

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ function areaquadrado(lado){
 
 console.log(areaquadrado(9));
 
-let areatrapezio=function(num1,num2,num3){
-    return (num1+num2)*num3/2;
+function areatrapezio(num1, num2, num3) {
+    return (num1 + num2) * num3 / 2;
 }
 
 console.log(areatrapezio(6,17,6));
@@ -32,3 +32,17 @@ let quantidade=function(numero,numero2,numeoro1){
 }
 
 document.write(quantidade);
+
+let nome=['andrew','caio','marina','mel','felipe_lin','joao','julia','felipe_mayer','arthur','monise','aaron','vinicius','maria','nicolas','luiz','lucas','izabela','giovana','danilo','lais','rhianne']
+
+let i=0;
+while(i<nome.length){
+console.log(i);
+console.log(nome[i]);
+i+1;
+}
+
+for (let i=0; i=nome.length;i++){
+    console.log(i);
+    console.log(nome[i]);
+}
